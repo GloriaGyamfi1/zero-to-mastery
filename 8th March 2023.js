75debeb42d46920cadec44c8b355cbd87f8a6fe2
GloriaGyamfi1 <120523017+GloriaGyamfi1@users.noreply.github.com>
2023-03-22 19:40:38 +0000

    Add files via upload

--- 8th March 2023.js
+++ 8th March 2023.js
@@ -1,14 +1,18 @@
 display = document.getElementById ("display")
 // display.innerHTML ="calcus"
-
+digits= "1234567890"
+operands= "+/-*"
 for (button of document.getElementsByTagName ("button")) {
     button.addEventListener ("click", function(ev){
       text =  ev.target.innerText
+      if (operands.includes(text) && !digits.includes(display.innerHTML.at(-1))) {
+        return
+      }
         if (text==="clear") {
             display.innerHTML=""
         }
         else if (text==="=") { 
-            display.innerHTML= eval(display.innerHTML)
+            display.innerHTML= eval(display.innerHTML) || ""
         }
         else {
             display.innerHTML += text  
